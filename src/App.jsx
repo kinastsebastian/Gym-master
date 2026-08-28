6d72359d7ab7ea56430127b387d0204faf00858c
Sebastian <kinastsebastian@gmail.com>
2026-08-28 17:03:27 -0400

    more bug lapiz

--- src/App.jsx
+++ src/App.jsx
@@ -514,9 +514,11 @@ const moverEjercicio = async (indexActual, direccion, e) => {
                     
                     {/* BOTONES (LÁPIZ Y X) */}
                     <div className="flex flex-shrink-0 items-center">
-                      <button onClick={(e) => editarNombreEjercicio(ej, e)} className="text-zinc-500 hover:text-blue-400 text-xs px-2" title="Editar nombre">
-                        ✏️
-                      </button>
+                      <button onClick={(e) => editarNombreEjercicio(ej, e)} className="text-zinc-600 hover:text-red-500 transition-colors px-2" title="Editar nombre">
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-3.5 h-3.5">
+    <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+  </svg>
+</button>
                       <button onClick={(e) => { e.preventDefault(); eliminarEjercicio(ej.id, e); }} className="text-zinc-600 hover:text-red-500 text-xs px-2" title="Eliminar">
                         ✕
                       </button>
