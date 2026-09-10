7db9034eeca4ba88ec8716938b7dcb40e112bd11
Sebastian <kinastsebastian@gmail.com>
2026-09-09 23:17:13 -0300

    trying fix

--- src/App.jsx
+++ src/App.jsx
@@ -631,7 +631,7 @@ const moverEjercicio = async (indexActual, direccion, e) => {
 
         {tabActiva === 'entrenar' && (
           <div className="animate-fade-in">
-   <div className="bg-zinc-900 rounded-sm p-1.5 mb-4 border border-zinc-800">
+  <div className="bg-zinc-900 rounded-sm p-1.5 mb-4 border border-zinc-800">
               {!creandoCategoria ? (
                 <div className="flex gap-1.5">
                   <select 
@@ -648,16 +648,6 @@ const moverEjercicio = async (indexActual, direccion, e) => {
                   >
                     +
                   </button>
-                  {/* NUEVO BOTÓN DE BASURERO */}
-                  {tiposRutina.length > 0 && (
-                    <button 
-                      onClick={eliminarCategoria}
-                      className="bg-zinc-950 border border-zinc-800 text-zinc-600 hover:text-red-600 hover:border-red-900/50 w-10 rounded-sm font-black flex items-center justify-center transition-colors shadow-[0_0_10px_rgba(0,0,0,0.5)]"
-                      title="Eliminar Rutina Actual"
-                    >
-                      <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" /></svg>
-                    </button>
-                  )}
                 </div>
               ) : (
                 <form onSubmit={agregarNuevaCategoria} className="flex gap-1.5 animate-fade-in">
